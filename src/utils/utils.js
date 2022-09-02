3aefd2a23e742dec138ce0d8f99237f5db67235a
wxShawn <1806394760@qq.com>
2022-09-02 10:26:50 +0800

    过滤掉pre标签中的内容

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,7 @@
 // html转文本
 export const html2Text = (html) => {
+  const preReg = /<pre>[\s\S]*<\/pre>/g;
   const regexp = /<[^>]+>/g;
-  const text = html.replace(regexp, '');
+  const text = html.replace(preReg, '').replace(regexp, '');
   return text;
 }
